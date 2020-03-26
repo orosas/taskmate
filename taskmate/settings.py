a1e63b99f26bf9dc597a42a6db7632c9789f8fa2
Omar Rosas <jorosas@gmail.com>
2020-03-26 17:57:29 -0600

    settings.py con configuración para Heroku

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -154,9 +154,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles') # Omar: Configuración para static files para deployment
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles') # Omar: Configuración para static files para deployment
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4' # Omar: Configuración de crispy forms
 
